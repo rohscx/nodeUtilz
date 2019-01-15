ff31463a618e13684eb6f4b88cd9275883edf9ff
rohscx <rohscx@gmail.com>
2019-01-15 11:44:40 -0500

    esLint formating suggestions implemented

--- box/putBoxFile.js
+++ box/putBoxFile.js
@@ -1,63 +1,71 @@
-const logCapture = require('../debug/logCapture.js');
-module.exports = function putBoxFile (BoxSDK,boxSecurityObject,boxAppUserId,folderId,fileName,stream) {
-  const myLogs = new logCapture();
-  const myErrorLogs = new logCapture();
+const LogCapture = require('../debug/logCapture.js');
+module.exports = function putBoxFile(BoxSDK, boxSecurityObject, boxAppUserId, folderId, fileName, stream) {
+  const myLogs = new LogCapture();
+  const myErrorLogs = new LogCapture();
 
-  function getFileId (fileData,fileName) {
-    return fileData.reduce((n,o) => {
-      if (o[fileName]) {
-        n = o[fileName];
-      } else {
-        n = "unable to locate fileId";
+  /**
+   * sends file to box.
+   * @param {obj} fileData file data object.
+   * @param {string} fileName name of the file.
+   * @return {int} The sum of the two numbers.
+   */
+  function getFileId(fileData, fileName) {
+    let fileId = '';
+    for (file in fileData) {
+      if (fileData[file].hasOwnProperty(fileName)) {
+        const fileObject = fileData[file];
+        fileId = fileObject[fileName];
       }
-      return n;
-    },"");
+    }
+    return fileId;
   }
 
-  const duplicateFileNames = () => {
-
-  }
 
   const sdk = new BoxSDK({
-  	clientID: boxSecurityObject.boxAppSettings.clientID,
-  	clientSecret: boxSecurityObject.boxAppSettings.clientSecret,
-  	appAuth: {
-  		keyID: boxSecurityObject.boxAppSettings.appAuth.publicKeyID,
-  		privateKey: boxSecurityObject.boxAppSettings.appAuth.privateKey,
-  		passphrase: boxSecurityObject.boxAppSettings.appAuth.passphrase
-  	}
+    clientID: boxSecurityObject.boxAppSettings.clientID,
+    clientSecret: boxSecurityObject.boxAppSettings.clientSecret,
+    appAuth: {
+      keyID: boxSecurityObject.boxAppSettings.appAuth.publicKeyID,
+      privateKey: boxSecurityObject.boxAppSettings.appAuth.privateKey,
+      passphrase: boxSecurityObject.boxAppSettings.appAuth.passphrase,
+    },
   });
   // Create auth client
   const appUserClient = sdk.getAppAuthClient('user', boxAppUserId);
 
   // Get some of that sweet, sweet data!
   appUserClient.users.get(appUserClient.CURRENT_USER_ID, null, function(err, currentUser) {
-    if(err) throw err;
-    myLogs.addLog('Hello, ' + currentUser.name + '!')
-    //console.log('Hello, ' + currentUser.name + '!');
-    myLogs.addLog(currentUser)
-    //console.log(JSON.stringify(currentUser))
+    if (err) throw err;
+    myLogs.addLog('Hello, ' + currentUser.name + '!');
+    // console.log('Hello, ' + currentUser.name + '!');
+    myLogs.addLog(currentUser);
+    // console.log(JSON.stringify(currentUser))
   });
   // Get more of that sweet, sweet data!
-  appUserClient.folders.getItems(folderId,{fields:"name,id,created_at"}).then(t => {
-    const directoryFilesObj = t.entries.map(({name,id}) =>  ({[name]:id}));
-    const directoryFilesObjKeys = directoryFilesObj.map(d => Object.keys(d));
+  appUserClient.folders.getItems(folderId, {fields: 'name,id,created_at'}).then((t) => {
+    const directoryFilesObj = t.entries.map(({name, id}) => ({[name]: id}));
+    const directoryFilesObjKeys = directoryFilesObj.map((d) => Object.keys(d));
     const directoryFileNames = [].concat(...directoryFilesObjKeys);
     if (directoryFilesObj) {
-      //const direcotryFilesKeys = Object.keys(directoryFilesObj);
+      // const direcotryFilesKeys = Object.keys(directoryFilesObj);
       // drops error if file already exists
       if (directoryFileNames.includes(fileName)) {
-        myErrorLogs.addLog(`\n\n File already exsists in Box folder! FileName: ${fileName} ID: ${getFileId(directoryFilesObj,fileName)} \n\n`)
-        return myErrorLogs.getLogs("FgRed")
+        myErrorLogs.addLog(`\n\n File already exsists in Box folder! FileName: ${fileName} ID: ${getFileId(directoryFilesObj, fileName)} \n\n`);
+        appUserClient.files.uploadNewFileVersion(getFileId(directoryFilesObj, fileName), stream).then((t) => {
+          myLogs.addLog(`Successfully Up Loaded New Version of file: ${fileName} to box:\n`);
+          myLogs.addLog(t);
+          // console.log(`Uploaded to box:\n${JSON.stringify(t)}`)
+          return myLogs.getLogs('Reset');
+        });
+        return myErrorLogs.getLogs('FgRed');
       }
     }
     // continues if the file does not exsist
-    appUserClient.files.uploadFile(folderId, fileName, stream).then(t => {
+    appUserClient.files.uploadFile(folderId, fileName, stream).then((t) => {
       myLogs.addLog(`Successfully Uploaded to box:\n`);
       myLogs.addLog(t);
-      //console.log(`Uploaded to box:\n${JSON.stringify(t)}`)
-      return myLogs.getLogs("Reset")
-    })
-  })
-
+      // console.log(`Uploaded to box:\n${JSON.stringify(t)}`)
+      return myLogs.getLogs('Reset');
+    });
+  });
 };
