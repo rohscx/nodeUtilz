38301104d5222f9080815432bf870c64f6b05619
rohscx <rohscx@gmail.com>
2019-03-05 09:17:38 -0500

    Debug fix. Object => logKeys. Cert Properties no longer caches certs.

--- cusFun/hostCertProperties.js
+++ cusFun/hostCertProperties.js
@@ -1,20 +1,27 @@
 const https = require('https');
+const Agent = require('https').Agent;
 
+// disabling session cache means performing certificate handshake on every request.
+// The Agent constructor takes a maxCachedSessions property. We’ll set it to 0 to prevent caching.
 /*
 nodeUitlz.getHostCertProperties('agaiseadpr01.fpicore.fpir.pvt')
   .then(console.log)
 */
-module.exports = function(host, debug = {debug: false}) {
+module.exports = function(host, debug = {logKeys: false}) {
   return new Promise((resolve) => {
     const options = {
       host: host,
       port: 443,
       method: 'GET',
       rejectUnauthorized: false,
+      // Disable session caching
+      agent: new Agent({
+        maxCachedSessions: 0,
+      }),
     };
     const req = https.request(options, function(res) {
+      if (debug.logKeys === true) console.log(Object.keys(res.connection.getPeerCertificate()));
       resolve(res.connection.getPeerCertificate());
-      if (debug = true) console.log(Object.keys(res.connection.getPeerCertificate()));
     });
     req.end();
   });
--- cusFun/sha1Hash.js
+++ cusFun/sha1Hash.js
@@ -7,11 +7,11 @@ nodeUitlz.readFile('./agaprimepr01.fpicore.fpir.pvt.020416.cert.der','utf8')
     .then(t => console.log(nodeUitlz.getSha1Hash(t)))
 */
 
-module.exports = function(cert, debug = {debug: false}) {
+module.exports = function(cert, debug = {logKeys: false}) {
   const shasum = crypto.createHash('sha1');
   const der = Buffer.from(cert, 'base64').toString('binary');
   shasum.update(der);
   const sha1Data = {sha1: shasum.digest('hex')};
-  if (debug = true) console.log(JSON.stringify(sha1Data));
+  if (debug.logKeys === true) console.log(JSON.stringify(sha1Data));
   return sha1Data;
 };
