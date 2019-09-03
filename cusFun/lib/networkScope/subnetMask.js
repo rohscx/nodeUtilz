a1afc0976bdc202ab71c7aa41fc028bd9a8cc0b4
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2019-09-02 21:31:09 -0400

    updates to network calculator

--- cusFun/lib/networkScope/subnetMask.js
+++ cusFun/lib/networkScope/subnetMask.js
@@ -6,6 +6,7 @@ module.exports = function subnetMask (number){
   // Using the prefix find the quotoen, remainder and modulus
   const maskPartA = (data) => {
     const prefix = data;
+    if (prefix < 8) return {quotient: 1, remainder: 1, modulus: prefix};
     const octents = 8;
     const rawQuotient = prefix / octents;
     const remainder = rawQuotient % 1;
@@ -14,10 +15,11 @@ module.exports = function subnetMask (number){
     return {quotient, remainder, modulus};
   };
   const {quotient, remainder, modulus} = maskPartA(n);
+  
   // Generate Octents with available data
   const generateLeftSide = Array.from(Array(quotient)).map((d) => Array.from(Array(8)).fill(1, 0, 8).join(''));
   const generateRightSide = Array(8).fill(1).map((d,i) => i < modulus ? 1 : 0).join('');
-  const generateSides = [...generateLeftSide, generateRightSide ];
+  const generateSides = n < 8 ? [ generateRightSide ] : [...generateLeftSide, generateRightSide ]  ;
 
   // Fill in any empty octents if octent Lenth < 4
   const octentLength = generateSides.length;
