0b5744c7022ffdfd9631cb48215aadf46b1e0cef
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2019-09-03 12:38:52 -0400

    Updated string parsers to accept more seperators

--- cusFun/parseMacFromString.js
+++ cusFun/parseMacFromString.js
@@ -2,7 +2,7 @@
 
 module.exports = function(data, options = {}) {
   const macs = (string) => {
-    return string.split('\n')
+    return string.split(/[\s,\n]/)
         .map((d) => d.split(' ').filter((f) => f.search(RegExp(/(([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9A-Fa-f]{4}[.:-]){2})/)) != -1))
         .filter((f )=> f.length > 0).map((d) => d.join(''));
   };
