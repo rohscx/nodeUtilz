acaa8842d70f00d63ae07b4f9201923721ae29f0
Foster <Erou.Foster@financialpartners.com>
2019-09-25 12:07:44 -0400

    qrCode method update. Added canvas

--- qrcode/qrCode.js
+++ qrcode/qrCode.js
@@ -1,8 +1,26 @@
 const QRCode = require('qrcode');
+const { createCanvas } = require('canvas');
 
 module.exports = function (data, options = {output:'terminal',qrOptions:{}}) {
     return new Promise((resolve, reject) => {
         const {output, qrOptions} = options;
+        const canvas = createCanvas(200, 200);
+        const htmlGenerator = (htmlElement) => {
+            htmlElement
+            return `
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <meta charset="UTF-8">
+                        <title>qrCode</title>
+                    </head>
+                <body>
+                    ${htmlElement}
+                </body>
+                </html>
+            `;
+
+        };
         switch (output) {
             case "terminal":
                 QRCode.toString(data, {type:'terminal'})
@@ -16,6 +34,18 @@ module.exports = function (data, options = {output:'terminal',qrOptions:{}}) {
                     reject(err)
                 })
                 break;
+            case "canvas":
+                    QRCode.toCanvas(canvas, data, qrOptions)
+                    .then(url => {
+                        // debug
+                        console.log(url)
+                        resolve(htmlGenerator(url))
+                    })
+                    .catch(err => {
+                        console.error(err)
+                        reject(err)
+                    })
+                    break;
         
             default:
                 QRCode.toDataURL(data, qrOptions)
