c581c4f3bf5f605a10b22d8612740125555c5813
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2020-03-04 15:04:50 -0500

    bugfix

--- request/asyncRequest.js
+++ request/asyncRequest.js
@@ -6,7 +6,7 @@ module.exports = function(optionsObj) {
   return new Promise(resolve => {
     request(options, function(error, response, body) {
       // if there is an error wait 5 seconds and retry the request. Poor mans retry
-      if (error.code === 'ETIMEDOUT') {
+      if (error.message.code === 'ETIMEDOUT') {
         return setTimeout(() => {
           request(options, function(error, response, body) {
             if (error)
