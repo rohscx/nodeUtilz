ebb899b24e14b30f0033a46e46df2e66b9588c27
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2020-03-31 07:47:30 -0400

    added options for verbose debug

--- cusFun/csvFromJson.js
+++ cusFun/csvFromJson.js
@@ -1,15 +1,16 @@
 const Json2csvParser = require('json2csv').Parser;
 
-module.exports = function(data, fields, unwind) {
+module.exports = function(data, fields, options = {debug:false, unwind:false}) {
+  const {debug, unwind} = options;
   return new Promise((resolve, reject) => {
-    const opts = { fields, unwind };
+    //const opts = { fields, unwind };
     try {
       const json2csvParser = new Json2csvParser(opts);
       const csv = json2csvParser.parse(data);
-      console.log(csv);
+      if (debug) console.log(csv);
       resolve(csv);
     } catch (err) {
-      console.error(err);
+      if (debug) console.error(err);
       reject(err);
     }
   });
